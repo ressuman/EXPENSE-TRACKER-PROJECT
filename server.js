557f98741b8d195f4501c7dbb092c9c76be93f78
Richard Essuman <ressuman001@gmail.com>
2024-05-27 05:55:50 +0000

    added node modules to the gitignore

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
@@ -22,6 +23,14 @@ if (process.env.NODE_ENV === "development") {
 }
 
 app.use("/api/v1/transactions", transactions);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/dist"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5100;
 
